118028cb1408d4b6c9b465633224c5e59a6f5066
mishabeskin <mishabeskin@gmail.com>
2021-03-11 14:18:57 +0200

    small changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('demoAngularProject app is running!');
+    expect(page.getTitleText()).toEqual('WebPalsProject app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demoAngularProject'`, () => {
+  it(`should have as title 'WebPalsProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demoAngularProject');
+    expect(app.title).toEqual('WebPalsProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demoAngularProject app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('WebPalsProject app is running!');
   });
 });
